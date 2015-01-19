e2c952c8afb6ae16b2e67e8c85a2983b9b2a67ec
andrew <andrew901@sjccs142.saintjoe.edu>
2015-01-19 13:45:36 -0500

    First outline of program.

---
+++ grades.js
@@ -0,0 +1,28 @@
+//Andrew Klump
+//1/19/2015
+//create a grades object that stores a set of student grades in an object. Provide a function of adding a grade and a function for displaying the students grade average
+
+var print = function(args) {
+    console.log(args);
+}
+
+
+
+//constructor for student grade object
+
+
+
+
+//grade average function
+
+
+
+
+
+//Adding a grade function
+
+
+
+
+
+print("Hello");
